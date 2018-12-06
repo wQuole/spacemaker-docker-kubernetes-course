0edf190ae12749c1e82338c034686dd8f43c8654
Håkon Åmdal <hakon@aamdal.com>
2018-12-06 15:43:28 +0100

    dns and such

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,22 @@
 provider "google-beta" {
-  project = "spacemaker-kubernetes-workshop"
-  region  = "europe-west1"
-  zone    = "europe-west1-b"
+  project = "${var.google_project}"
+  region  = "${var.google_region}"
+  zone    = "${var.google_zone}"
+}
+
+provider "aws" {
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+  region     = "eu-west-1"
 }
 
 data "google_container_engine_versions" "region" {
-  project = "spacemaker-kubernetes-workshop"
-  zone    = "europe-west1-b"
+  project = "${var.google_project}"
+  zone    = "${var.google_zone}"
 }
 
 resource "google_container_cluster" "cluster" {
-  name               = "abakus"
+  name               = "${var.cluster_name}"
   provider           = "google-beta"
   initial_node_count = 1
   min_master_version = "${data.google_container_engine_versions.region.latest_master_version}"
@@ -22,4 +28,19 @@ resource "google_container_cluster" "cluster" {
       "node_version",
     ]
   }
+}
+
+resource "google_compute_address" "load_balancer" {
+  project = "${var.google_project}"
+  name    = "k8s-${var.cluster_name}-load-balancer"
+  region  = "${var.google_region}"
+}
+
+resource "aws_route53_record" "load_balancer_a" {
+  zone_id = "${var.route53_zone_id}"
+  name    = "${var.cluster_name}.${var.route53_domain_name}"
+  type    = "A"
+  ttl     = 3600
+
+  records = ["${google_compute_address.load_balancer.address}"]
 }
---
+++ terraform/vars.tf
@@ -0,0 +1,26 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+variable "google_project" {
+  default = "spacemaker-kubernetes-workshop"
+}
+
+variable "google_region" {
+  default = "europe-west1"
+}
+
+variable "google_zone" {
+  default = "europe-west1-b"
+}
+
+variable "cluster_name" {
+  default = "abakus"
+}
+
+variable "route53_zone_id" {
+  default = "Z23LWAXUQ9PS6E"
+}
+
+variable "route53_domain_name" {
+  default = "spacemaker.ai"
+}
