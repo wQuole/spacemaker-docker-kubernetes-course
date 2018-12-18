7e03d124f558690100b1e8bd55da5009a599ce0e
larseen <kristoffer@larsen.so>
2018-12-18 14:26:13 +0100

    add node example service

--- example-services/node/app.js
+++ example-services/node/app.js
@@ -3,5 +3,11 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  console.log("This is a request");
+  res.json([
+    { x: 0, y: 0, dx: 20, dy: 20, dz: 20 },
+    { x: 40, y: 40, dx: 20, dy: 20, dz: 40 }
+  ]);
 });
+
+module.exports = app;
