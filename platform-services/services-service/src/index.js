2f6345028251b557c7846885cbed18efea90ca10
larseen <kristoffer@larsen.so>
2018-12-18 12:34:42 +0100

    Add simple serivces-service which find services and proxies to a service

---
+++ platform-services/services-service/src/index.js
@@ -0,0 +1,15 @@
+import log from "@spacemakerai/log";
+import app from "./app";
+import config from "./config";
+
+log.info("server starting", {
+  port: config.port,
+  env: config.env
+});
+
+app.listen(config.port, () => {
+  log.info("server listening", {
+    port: config.port,
+    env: config.env
+  });
+});
