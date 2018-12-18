fb21cf71b07e7e80351c3d602c4ddee254654420
Håkon Åmdal <hakon@aamdal.com>
2018-12-18 14:37:58 +0100

    validate-buildings overlap

--- platform-services/validation-service/pkg/server/main.go
+++ platform-services/validation-service/pkg/server/main.go
@@ -12,7 +12,6 @@ import (
 // StartServer starts the HTTP server at the specified port
 func StartServer(port int) {
 	http.HandleFunc("/", handleRoot)
-	http.HandleFunc("/validate-building", handleValidateBuilding)
 	http.HandleFunc("/validate-buildings", handleValidateBuildings)
 	log.Printf("Starting server at port %v...", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%v", port), nil))
@@ -21,28 +20,6 @@ func StartServer(port int) {
 func handleRoot(w http.ResponseWriter, r *http.Request) {
 	log.Printf("Received request %v...", r)
 	fmt.Fprint(w, "Hello word")
-}
-
-func handleValidateBuilding(w http.ResponseWriter, r *http.Request) {
-	log.Printf("Received request %v...", r)
-	if r.Method != "POST" {
-		http.Error(w, "Invalid request method", http.StatusMethodNotAllowed)
-		return
-	}
-	decoder := json.NewDecoder(r.Body)
-	var b model.Building
-	if err := decoder.Decode(&b); err != nil {
-		http.Error(w, "Could not deserialize building", http.StatusBadRequest)
-		return
-	}
-	isValid, errorMessage := b.IsValid()
-	response := buildingValidationResult{
-		IsValid:      isValid,
-		ErrorMessage: errorMessage,
-	}
-	responseJSON, _ := json.Marshal(response)
-	w.WriteHeader(http.StatusOK)
-	w.Write(responseJSON)
 }
 
 func handleValidateBuildings(w http.ResponseWriter, r *http.Request) {
@@ -57,13 +34,36 @@ func handleValidateBuildings(w http.ResponseWriter, r *http.Request) {
 		http.Error(w, "Could not deserialize building", http.StatusBadRequest)
 		return
 	}
-	var response []buildingValidationResult
+	var validationResults []buildingValidationResult
 	for _, b := range buildings {
 		isValid, errorMessage := b.IsValid()
-		response = append(response, buildingValidationResult{
+		validationResults = append(validationResults, buildingValidationResult{
 			IsValid:      isValid,
 			ErrorMessage: errorMessage,
 		})
+	}
+	isValid := true
+	errorMessage := ""
+	// check if anyone has validation errors
+	for _, b := range validationResults {
+		if !b.IsValid {
+			isValid = false
+			errorMessage = "One or more buildings had validation errors"
+		}
+	}
+	// check for overlap
+	for _, b1 := range buildings {
+		for _, b2 := range buildings {
+			if b1 != b2 && b1.Overlaps(&b2) {
+				isValid = false
+				errorMessage = "One or more buildings overlap"
+			}
+		}
+	}
+	response := validationResult{
+		IsValid:           isValid,
+		ErrorMessage:      errorMessage,
+		ValidationResults: validationResults,
 	}
 	responseJSON, _ := json.Marshal(response)
 	w.WriteHeader(http.StatusOK)
--- platform-services/validation-service/pkg/server/responses.go
+++ platform-services/validation-service/pkg/server/responses.go
@@ -1,5 +1,11 @@
 package server
 
+type validationResult struct {
+	IsValid           bool                       `json:"isValid"`
+	ErrorMessage      string                     `json:"errorMessage"`
+	ValidationResults []buildingValidationResult `json:"validationResults"`
+}
+
 type buildingValidationResult struct {
 	IsValid      bool   `json:"isValid"`
 	ErrorMessage string `json:"errorMessage"`
