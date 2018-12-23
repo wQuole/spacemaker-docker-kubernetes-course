5566be2aef3c9f3ff3d67ff100641d219e1280d1
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2018-12-23 18:56:03 +0100

    refactor: use local coordinate system inside block

--- web/tile/src/index.js
+++ web/tile/src/index.js
@@ -55,42 +55,58 @@ function run() {
     return gridHelper;
   }
 
+  function computeBlockPosition({ tileX, tileY }) {
+    let nextTileX = tileX + tileSizeX + borderX;
+    let nextTileY = tileY;
+    if (nextTileX + tileSizeX > maxSizeX) {
+      nextTileX = 0;
+      nextTileY = tileY + tileSizeY + borderY;
+    }
+    return { nextTileX, nextTileY };
+  }
+
+  function createBuilding({ x, y, dx, dy, dz }) {
+    if (dx < 0) dx = 1;
+    if (dy < 0) dy = 1;
+    if (dz < 0) dx = 1;
+    if (dx + x > tileSizeX) {
+      dx = tileSizeX - x;
+    }
+    if (dy + y > tileSizeY) {
+      dy = tileSizeY - y;
+    }
+
+    const geometry = new THREE.BoxBufferGeometry(dx, dy, dz);
+    const material = new THREE.MeshBasicMaterial({
+      color: new THREE.Color(0xcccccc)
+    });
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.position.x = x + dx / 2;
+    mesh.position.y = y + dy / 2;
+    mesh.position.z = dz / 2;
+
+    return mesh;
+  }
+
   function createTiles(tiles) {
     let tileX = 0,
       tileY = 0;
     const city = new THREE.Group();
     for (let [service, tile] of Object.entries(tiles)) {
       const block = new THREE.Group();
+
+      block.position.x = tileX;
+      block.position.y = tileY;
+      block.position.z = 0;
+
       for (let building of tile) {
-        let { x, y, dx, dy, dz } = building;
-
-        if (dx < 0) dx = 1;
-        if (dy < 0) dy = 1;
-        if (dz < 0) dx = 1;
-        if (dx + x > tileSizeX) {
-          dx = tileSizeX - x;
-        }
-        if (dy + y > tileSizeY) {
-          dy = tileSizeY - y;
-        }
-
-        const geometry = new THREE.BoxBufferGeometry(dx, dy, dz);
-        const material = new THREE.MeshBasicMaterial({
-          color: new THREE.Color(0xcccccc)
-        });
-        const mesh = new THREE.Mesh(geometry, material);
-        mesh.position.x = tileX + (x % tileSizeX) + dx / 2;
-        mesh.position.y = tileY + (y % tileSizeY) + dy / 2;
-        mesh.position.z = dz / 2;
-
-        block.add(mesh);
+        console.log({ tileX, tileY });
+        block.add(createBuilding(building));
       }
 
-      tileX += tileSizeX + borderX;
-      if (tileX + tileSizeX > maxSizeX) {
-        tileX = 0;
-        tileY += tileSizeY + borderY;
-      }
+      let { nextTileX, nextTileY } = computeBlockPosition({ tileX, tileY });
+      tileX = nextTileX;
+      tileY = nextTileY;
 
       city.add(block);
     }
