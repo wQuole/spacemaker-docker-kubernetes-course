eee049a3eaac4386e7f28ca4d6585bd3012597a2
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2019-01-01 11:55:36 +0100

    docs: document web/tile docker

--- web/tile/docker-entrypoint.sh
+++ web/tile/docker-entrypoint.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 set -e
 
-LOCAL_PORT=${LOCAL_PORT:-3000}
+# This script is executed when running `docker run ...` just before the CMD in
+# the docker file. It checks for the LOCAL_MODE enviroment variable and sets up
+# a nginx configuration with a proxy to the docker host if the variable is true.
+
+LOCAL_PORT=${LOCAL_PORT:-3000} # Set local port enviroment variable default value
 
 if [ $LOCAL_MODE ]; then
   echo "Setting up local service at port $LOCAL_PORT"
