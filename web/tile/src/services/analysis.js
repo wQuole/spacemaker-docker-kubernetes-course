10ca70047a2924ffa58a19af62489b6c20cbabc9
Håvard Wormdal Høiby <havardwhoiby@gmail.com>
2019-01-07 19:17:03 +0100

    feat: analyse

--- web/tile/src/services/analysis.js
+++ web/tile/src/services/analysis.js
@@ -1,3 +1,12 @@
 export async function analyse(block) {
   return JSON.stringify(block).length;
+  /*
+  const response = await fetch("/analysis-service", {
+    method: "POST",
+    headers: { Accept: "application/json", "Content-Type": "application/json" },
+    body: JSON.stringify(block)
+  });
+
+  return response.json();
+  */
 }
--- web/tile/webpack.config.js
+++ web/tile/webpack.config.js
@@ -10,7 +10,16 @@ module.exports = {
       },
       "/validation-service": {
         target: "http://abakus.spacemaker.ai",
+        // target: "http://localhost:5001",
         changeOrigin: true
+      },
+      "/analysis-service": {
+        // target: "http://abakus.spacemaker.ai",
+        target: "http://localhost:5002",
+        changeOrigin: true,
+        pathRewrite: {
+          "/analysis-service": ""
+        }
       }
     }
   }
